916205defb95131917af479d07cf5b0e7371d765
Gabriel Sferra <gabriel.sferra@dev.auaha.com.br>
2023-06-28 16:26:28 -0300

    WIP: Header change

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -58,7 +58,7 @@ function Header({
   return (
     <>
       <header style={{ height: headerHeight }}>
-        <div class="bg-base-100 fixed w-full z-50">
+        <div class="bg-gradient-to-b from-fuchsia-500 to-fuchsia-100  bg-orange fixed w-full z-50">
           <Alert alerts={alerts} />
           <Navbar items={navItems} searchbar={searchbar} />
         </div>
